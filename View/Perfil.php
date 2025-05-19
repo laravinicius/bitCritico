bf243c96aa9ec6f23d8845f05e46efb7467e8ff8
Vinicius Lara <103464054+laravinicius@users.noreply.github.com>
2025-05-19 14:57:14 -0300

    Update Perfil.php

--- View/Perfil.php
+++ View/Perfil.php
@@ -3,7 +3,7 @@ session_start();
 
 // Verifica se o usuário está logado
 if (!isset($_SESSION['id_usuario'])) {
-    header('Location: /bitCritico/index.php');
+    header('Location: ../index.php');
     exit();
 }
 
